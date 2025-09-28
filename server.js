f2dbb83b9eff5d6dd3fc638d965aa9a69dafae75
engr-tunde <mudashir.tunde.abdul@gmail.com>
2025-09-28 12:11:17 +0100

    thel15

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+const app = express();
+const port = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("/{*path}", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
